1570f4d8b8f806fe3561e7bf3ac6525c83d8bd07
jjaneyxx <hmyeonge@gmail.com>
2024-12-18 09:03:37 +0900

    fix: 비밀번호가 먼저 입력될 때 버튼 색상 변경 오류 수정

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ const userEmail = document.getElementById("user-email");
 const userPwd = document.getElementById("user-password");
 const loginBtn = document.getElementById("loginBtn");
 
-let emailFilled = true;
+let emailFilled = false;
 
 userEmail.addEventListener("blur", () => {
-  if (isValidEmail(userEmail.value)) {
-    userEmail.value !== "" ? (emailFilled = true) : (emailFilled = false);
-  }
+  // 입력한 이메일 필드가 비어있는지 확인
+  userEmail.value !== "" ? (emailFilled = true) : (emailFilled = false);
 });
 
+// 비밀번호 입력 시,  조건에 따라 버튼 색상을 변경
 userPwd.addEventListener("input", () => {
+  // 이메일이 입력되어 있고 비밀번호가 8자 이상인 경우
   if (emailFilled && userPwd.value.length >= 8) {
     loginBtn.classList.add("color-blue");
   }
