af1537f4314a14e4e0d6415bc768af0854b9620c
Seokjun Kang <kangsj0327@gmail.com>
2024-12-23 14:14:59 +0900

    feat: login 페이지 로그인 정보  미입력시 로그인 버튼 비활성화 기능 추가

--- login/script.js
+++ login/script.js
@@ -7,7 +7,8 @@ const isInputFilled = (input) => input.value.trim() !== "";
 const updateInputState = () => {
   const isFormFilled =
     isInputFilled(emailInput) && isInputFilled(passwordInput);
-  submitButton.classList.toggle("active", isFormFilled);
+  submitButton.classList.toggle("active", isFormFilled); // 버튼 스타일 변경
+  submitButton.disabled = !isFormFilled; // 버튼 활성/비활성
 };
 
 [emailInput, passwordInput].forEach((input) => {
