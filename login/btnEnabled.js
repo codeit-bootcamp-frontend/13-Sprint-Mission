7662794506723f5373b8381b8ab3968c5846dc08
heejin <heejin0857@naver.com>
2024-12-24 01:16:46 +0900

    Feat : login validate function

--- login/btnEnabled.js
+++ login/btnEnabled.js
@@ -1,14 +1,75 @@
+import { validateEmail, validateEmpty, validatePassword, errorMessage } from "../utils/validate.js";
+
 function loginActive() {
     const emailTarget = document.getElementById("email");
     const passwordTarget = document.getElementById("password");
     const loginBtn = document.querySelector(".login-btn");
 
-    const inputValue = () => {
-        loginBtn.disabled = !(emailTarget.value.trim() && passwordTarget.value.trim());
+    const errMessage = (input, errorM) => {
+        const err = input.parentElement.parentElement.querySelector(".error-message");
+        if (errorM) {
+            input.classList.add("error");
+            err.textContent = errorM;
+        } else {
+            input.classList.remove("error");
+            err.textContent = "";
+        }
     };
 
-    emailTarget.addEventListener("input", inputValue);
-    passwordTarget.addEventListener("input", inputValue);
+    const emailValidate = () => {
+        const value = emailTarget.value.trim();
+        if (!validateEmpty(value)) {
+            errMessage(emailTarget, errorMessage.email.empty);
+            return false;
+        }
+
+        if (!validateEmail(value)) {
+            errMessage(emailTarget, errorMessage.email.invalid);
+            return false;
+        }
+
+        errMessage(emailTarget, "");
+        return true;
+    };
+
+    const passwordValidate = () => {
+        const value = passwordTarget.value.trim();
+        if (!validateEmpty(value)) {
+            errMessage(passwordTarget, errorMessage.password.empty);
+            return false;
+        }
+
+        if (!validatePassword(value)) {
+            errMessage(passwordTarget, errorMessage.password.invalid);
+            return false;
+        }
+
+        errMessage(passwordTarget, "");
+        return true;
+    };
+
+    const loginValidate = () => {
+        const isEmailValid =
+            !emailTarget.classList.contains("error") &&
+            validateEmpty(emailTarget.value) &&
+            validateEmail(emailTarget.value);
+        const isPasswordValid = !passwordTarget.classList.contains("error") && validatePassword(passwordTarget.value);
+
+        loginBtn.disabled = !(isEmailValid && isPasswordValid);
+    };
+
+    emailTarget.addEventListener("blur", emailValidate);
+    passwordTarget.addEventListener("blur", passwordValidate);
+
+    emailTarget.addEventListener("input", loginValidate);
+    passwordTarget.addEventListener("input", loginValidate);
+
+    loginBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (!loginBtn.disabled) {
+            window.location.href = "../items.html";
+        }
+    });
 }
 
 loginActive();
