280e9bb9199c8f08ccec95e54e4c6928b82b4bf1
heejin <heejin0857@naver.com>
2024-12-24 00:50:48 +0900

    Feat : password regex edit & error message object

--- utils/validate.js
+++ utils/validate.js
@@ -1,5 +1,5 @@
 const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-const PASSWORD_REGEX = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/;
+const PASSWORD_REGEX = /^(?=.*[a-zA-Z]).{8,}$/;
 
 export const validateEmail = (value) => {
     return EMAIL_REGEX.test(value);
@@ -15,4 +15,17 @@ export const validatePasswordCheck = (password1, password2) => {
 
 export const validateEmpty = (value) => {
     return value.trim() !== "";
+};
+
+export const errorMessage = {
+    email: {
+        empty: "이메일을 입력해주세요",
+        invalid: "잘못된 이메일 형식입니다",
+    },
+    password: {
+        empty: "비밀번호를 입력해주세요",
+        invalid: "비밀번호를 8자 이상 입력해주세요",
+        notEqual: "비밀번호가 일치하지 않습니다",
+    },
+    nickname: "닉네임을 입력해주세요",
 };
