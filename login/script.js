d23db97ca546754c19cd339137e5030372d3b322
Seokjun Kang <kangsj0327@gmail.com>
2024-12-23 14:48:03 +0900

    feat: signup 페이지 style과 script를 login 페이지와 똑같이 적용

--- login/script.js
+++ login/script.js
@@ -1,17 +1,15 @@
-const emailInput = document.querySelector("#email");
-const passwordInput = document.querySelector("#password");
+const formInputs = document.querySelectorAll(".form-field-input");
 const submitButton = document.querySelector(".form-button-submit");
 
 const isInputFilled = (input) => input.value.trim() !== "";
 
 const updateInputState = () => {
-  const isFormFilled =
-    isInputFilled(emailInput) && isInputFilled(passwordInput);
+  const isFormFilled = Array.from(formInputs).every(isInputFilled);
   submitButton.classList.toggle("active", isFormFilled); // 버튼 스타일 변경
   submitButton.disabled = !isFormFilled; // 버튼 활성/비활성
 };
 
-[emailInput, passwordInput].forEach((input) => {
+formInputs.forEach((input) => {
   input.addEventListener("input", updateInputState);
 });
 
