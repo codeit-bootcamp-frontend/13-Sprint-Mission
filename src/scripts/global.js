e9185122d850b87f1c0e904011a2b24bc7ead30e
Hyojun Yoon <hyoyoon@student.42seoul.kr>
2024-12-29 17:20:13 +0900

    Feat: add submit button

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -22,3 +22,26 @@ export function setVisibilityToggle(inputWrapperSelector) {
         })
     })
 }
+
+export function setButtonDisable(parent, buttonClass) {
+    const button = parent.querySelector(buttonClass);
+  
+    // 초기 상태 및 입력값 검증 함수
+    function validateInputs() {
+      const inputs = parent.querySelectorAll("input");
+      const allFilled = Array.from(inputs).every(input => input.value.trim() !== "");
+      button.disabled = !allFilled;
+    }
+
+    /*
+        초기 상태에 button을 disabled로 설정하였으나
+        안전성과 일관성을 위해 호출
+    */
+    validateInputs();
+  
+    parent.addEventListener("input", (event) => {
+      if (event.target.tagName === "INPUT") {
+        validateInputs();
+      }
+    });
+  }
--- src/scripts/pages/login.js
+++ src/scripts/pages/login.js
@@ -1,3 +1,6 @@
-import { setVisibilityToggle } from "/src/scripts/global.js";
+import { setVisibilityToggle, setButtonDisable } from "/src/scripts/global.js";
 
 setVisibilityToggle(".input-wrapper");
+
+const contentWrapper = document.querySelector(".content-wrapper");
+setButtonDisable(contentWrapper, ".primary-button");
