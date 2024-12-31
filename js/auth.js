262a266b256f5b2da6750d97b15dc9a3c900ebc8
Seokjun Kang <kangsj0327@gmail.com>
2024-12-31 17:24:41 +0900

    refactor(mentor): auth - 폼 제출 버튼 스타일 변경 코드 js -> css 선택자 변경

--- js/auth.js
+++ js/auth.js
@@ -3,16 +3,16 @@ import { isEmpty } from "./utils.js";
 const formInputs = document.querySelectorAll(".form-field-input");
 const submitButton = document.querySelector(".form-button-submit");
 
-const updateInputState = () => {
+const updateSubmitButton = () => {
   const isFormFilled = Array.from(formInputs)
     .map((input) => input.value)
     .every((string) => !isEmpty(string));
-  submitButton.classList.toggle("active", isFormFilled); // 버튼 스타일 변경
-  submitButton.disabled = !isFormFilled; // 버튼 활성/비활성
+
+  submitButton.disabled = !isFormFilled;
 };
 
 formInputs.forEach((input) => {
-  input.addEventListener("input", updateInputState);
+  input.addEventListener("input", updateSubmitButton);
 });
 
-updateInputState();
+updateSubmitButton();
