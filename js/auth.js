622926403501ed26ccab12712bfca97686bc6f2b
Seokjun Kang <kangsj0327@gmail.com>
2024-12-31 17:00:48 +0900

    refactor(mentor): 사용자 입력 유무 확인 함수 파라미터 변경

--- js/auth.js
+++ js/auth.js
@@ -1,10 +1,12 @@
+import { isEmpty } from "./utils.js";
+
 const formInputs = document.querySelectorAll(".form-field-input");
 const submitButton = document.querySelector(".form-button-submit");
 
-const isInputFilled = (input) => input.value.trim() !== "";
-
 const updateInputState = () => {
-  const isFormFilled = Array.from(formInputs).every(isInputFilled);
+  const isFormFilled = Array.from(formInputs)
+    .map((input) => input.value)
+    .every((string) => !isEmpty(string));
   submitButton.classList.toggle("active", isFormFilled); // 버튼 스타일 변경
   submitButton.disabled = !isFormFilled; // 버튼 활성/비활성
 };
