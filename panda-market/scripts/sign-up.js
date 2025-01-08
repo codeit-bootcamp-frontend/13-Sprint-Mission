353f802f29e983979c9d101cd30f73789461a286
hdh12191 <hdh12191@naver.com>
2025-01-08 23:31:48 +0900

    feat : 회원가입 validation 완료

--- panda-market/scripts/sign-up.js
+++ panda-market/scripts/sign-up.js
@@ -1,12 +1,22 @@
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
+const nicknameInput = document.querySelector("#nickname");
+const checkPasswordInput = document.querySelector("#checkPassword");
 const emailError = document.querySelector(".email.error");
 const passwordError = document.querySelector(".password.error");
+const checkPasswordError = document.querySelector(".checkPassword.error");
+const nicknameError = document.querySelector(".nickname.error");
 const loginBtn = document.querySelector(".login-btn");
 const emailInputWrapper = document.querySelector(".input-wrapper.email");
 const passwordInputWrapper = document.querySelector(".input-wrapper.password");
+const nicknameInputWrapper = document.querySelector(".input-wrapper.nickname");
+const checkPasswordInputWrapper = document.querySelector(
+  ".input-wrapper.checkPassword"
+);
 const ENTER_EMAIL = "이메일을 입력해주세요.";
+const ENTER_NICKNAME = "닉네임을 입력해주세요.";
 const ENTER_PASSWORD = "비밀번호를 입력해주세요.";
+const WRONG_CHECK_PASSWORD = "비밀번호가 일치하지 않습니다.";
 const WRONG_EMAIL = "잘못된 이메일 형식입니다";
 const PASSWORD_NEED_EIGHT_WORDS = "비밀번호를 8자 이상 입력해주세요.";
 const NO_ERROR_MESSAGE = "";
@@ -16,8 +26,18 @@ const toggleLoginButton = () => {
     emailInput.value && emailError.innerHTML === NO_ERROR_MESSAGE;
   const isPasswordValid =
     passwordInput.value && passwordError.innerHTML === NO_ERROR_MESSAGE;
+  const isNicknameValid =
+    nicknameInput.value && nicknameError.innerHTML === NO_ERROR_MESSAGE;
+  const isCheckPasswordValid =
+    checkPasswordInput.value &&
+    checkPasswordError.innerHTML === NO_ERROR_MESSAGE;
 
-  if (isEmailValid && isPasswordValid) {
+  if (
+    isEmailValid &&
+    isPasswordValid &&
+    isNicknameValid &&
+    isCheckPasswordValid
+  ) {
     loginBtn.classList.remove("disabled");
     loginBtn.disabled = false;
   } else {
@@ -25,7 +45,6 @@ const toggleLoginButton = () => {
     loginBtn.disabled = true;
   }
 };
-
 const emailValid = () => {
   if (!emailInput.value) {
     emailError.innerHTML = ENTER_EMAIL;
@@ -56,6 +75,31 @@ const passwordValid = () => {
   }
   toggleLoginButton();
 };
+const nicknameValid = () => {
+  if (!nicknameInput.value) {
+    nicknameError.innerHTML = ENTER_NICKNAME;
+    nicknameInputWrapper.style.border = "3px solid #F74747";
+  } else {
+    nicknameError.innerHTML = NO_ERROR_MESSAGE;
+    nicknameInputWrapper.style.border = "none";
+  }
+
+  toggleLoginButton();
+};
+
+const checkPasswordValid = () => {
+  if (passwordInput.value !== checkPasswordInput.value) {
+    checkPasswordError.innerHTML = WRONG_CHECK_PASSWORD;
+    checkPasswordInputWrapper.style.border = "3px solid #F74747";
+  } else {
+    checkPasswordError.innerHTML = NO_ERROR_MESSAGE;
+    checkPasswordInputWrapper.style.border = "none";
+  }
+
+  toggleLoginButton();
+};
 
 emailInput.addEventListener("blur", emailValid);
 passwordInput.addEventListener("blur", passwordValid);
+nicknameInput.addEventListener("blur", nicknameValid);
+checkPasswordInput.addEventListener("blur", checkPasswordValid);
