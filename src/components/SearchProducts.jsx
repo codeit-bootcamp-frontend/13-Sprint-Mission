ecd4faeaac9a10c604663e576f85544b771ea493
hdh12191 <hdh12191@naver.com>
2025-01-09 16:58:03 +0900

    feat : SearchProduct 컴포넌트 완료

--- src/components/SearchProducts.jsx
+++ src/components/SearchProducts.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import searchImg from "../assets/images/searchImg.svg";
+import styled from "styled-components";
 
 const SearchProducts = () => {
-  return <div>SearchProducts</div>;
+  return (
+    <SearchInputWrapper>
+      <img src={searchImg} alt="검색 이미지" />
+      <SearchInput type="text" placeholder="검색할 상품을 입력해주세요" />
+    </SearchInputWrapper>
+  );
 };
 
 export default SearchProducts;
+
+const SearchInputWrapper = styled.div`
+  display: flex;
+  width: 325px;
+  gap: 8px;
+  align-items: center;
+  background-color: #f3f4f6;
+  padding: 9px 16px;
+  border-radius: 12px;
+`;
+
+const SearchInput = styled.input`
+  border: none;
+  background-color: transparent;
+  min-width: 202px;
+  outline: none;
+`;
