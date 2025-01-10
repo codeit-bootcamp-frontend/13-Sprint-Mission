edb6c522710324b37f8df3ccfb4bbca9f4506a10
hdh12191 <hdh12191@naver.com>
2025-01-11 00:42:18 +0900

    feat : Pagination 컴포넌트 완료

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import styled from "styled-components";
 
 const Pagination = () => {
-  return <div>Pagination</div>;
+  return (
+    <PaginationWrapper>
+      <PaginationItem>{`<`}</PaginationItem>
+      <PaginationItem>1</PaginationItem>
+      <PaginationItem>2</PaginationItem>
+      <PaginationItem>3</PaginationItem>
+      <PaginationItem>4</PaginationItem>
+      <PaginationItem>5</PaginationItem>
+      <PaginationItem>{`>`}</PaginationItem>
+    </PaginationWrapper>
+  );
 };
 
 export default Pagination;
+
+const PaginationWrapper = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 43px 0 58px;
+  gap: 4px;
+`;
+const PaginationItem = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 40px;
+  border: 1px solid #e5e7eb;
+  color: #6b7280;
+  font-weight: 600;
+  background-color: transparent;
+
+  &:hover {
+    color: #f9fafb;
+    background-color: #2f80ed;
+    border-color: #2f80ed;
+    cursor: pointer;
+  }
+`;
