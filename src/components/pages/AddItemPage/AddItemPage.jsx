3b5ce2fb2f68a6ae0dc459954cbea1c9099a069c
heejin <heejin0857@naver.com>
2025-01-18 01:39:59 +0900

    Feat : add item component UI

--- src/components/pages/AddItemPage/AddItemPage.jsx
+++ src/components/pages/AddItemPage/AddItemPage.jsx
@@ -1,3 +1,83 @@
+import * as S from "./AddItemPage.styles";
+import Input from "../../common/Input/Input";
+import FileInput from "../../FileInput/FileInput";
+import x from "../../../assets/icons/delete.svg";
+import Tag from "../../Tag/Tag";
+
 export default function AddItemPage() {
-  return <></>;
+  const INPUT = [
+    {
+      label: "상품명",
+      name: "name",
+      type: "text",
+      placeholder: "상품명을 입력해주세요",
+      value: "",
+      isTextarea: false,
+    },
+    {
+      label: "상품 소개",
+      name: "description",
+      type: "text",
+      placeholder: "상품 소개를 입력해주세요",
+      style: { height: "282px" },
+      value: "",
+      isTextarea: true,
+    },
+    {
+      label: "판매가격",
+      name: "price",
+      type: "number",
+      placeholder: "판매 가격을 입력해주세요",
+      value: "",
+      isTextarea: false,
+    },
+    {
+      label: "태그",
+      name: "tag",
+      type: "text",
+      placeholder: "태그를 입력해주세요",
+      value: "",
+      isTextarea: false,
+    },
+  ];
+
+  const tag = ["티셔츠", "상의"];
+
+  return (
+    <S.AddItemContainer>
+      <S.AddItem>
+        <S.AddItemHeader>
+          <S.Add>상품 등록하기</S.Add>
+          <S.AddBtn>등록</S.AddBtn>
+        </S.AddItemHeader>
+        <S.InputContainer>
+          <S.AddImg>
+            <FileInput lable="상품 이미지" name="file" onChange={() => console.log("")} />
+            <S.Preview>
+              <S.PreviewImg />
+              <S.DeleteImg src={x} />
+            </S.Preview>
+          </S.AddImg>
+          {INPUT.map((i, idx) => (
+            <Input
+              key={idx}
+              label={i.label}
+              name={i.name}
+              type={i.type}
+              placeholder={i.placeholder}
+              value={i.value}
+              style={i.style}
+              isTextarea={i.isTextarea}
+              onChange={() => console.log("")}
+            />
+          ))}
+        </S.InputContainer>
+        <S.TagList>
+          {tag.map((t, idx) => (
+            <Tag key={idx} tag={t} />
+          ))}
+        </S.TagList>
+      </S.AddItem>
+    </S.AddItemContainer>
+  );
 }
---
+++ src/components/pages/AddItemPage/AddItemPage.styles.jsx
@@ -0,0 +1,84 @@
+import { styled } from "styled-components";
+
+export const AddItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const AddItem = styled.div`
+  width: 1200px;
+  display: flex;
+  flex-direction: column;
+  padding: 10px 0;
+`;
+
+export const AddItemHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 24px;
+`;
+
+export const Add = styled.h1`
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 32px;
+  color: var(--gray800);
+`;
+
+export const AddBtn = styled.button`
+  width: 72px;
+  height: 42px;
+  border-radius: 8px;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 26px;
+  background-color: var(--primary);
+  color: var(--gray100);
+  cursor: pointer;
+`;
+
+export const InputContainer = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+export const AddImg = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+  gap: 24px;
+`;
+
+export const Preview = styled.div`
+  display: flex;
+  position: relative;
+`;
+
+export const PreviewImg = styled.img`
+  width: 282px;
+  height: 282px;
+  background-color: black;
+  border-radius: 12px;
+`;
+
+export const DeleteImg = styled.img`
+  width: 22px;
+  height: 24px;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  cursor: pointer;:
+`;
+
+export const TagList = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  gap: 12px;
+  margin-top: 14px;
+`;
