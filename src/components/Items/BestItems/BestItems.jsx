6cceb5f13226d0a647c6aca53da2dbb87857eff0
heejin <heejin0857@naver.com>
2025-02-11 15:50:54 +0900

    Refactor : useResize custom hook

--- src/components/Items/BestItems/BestItems.jsx
+++ src/components/Items/BestItems/BestItems.jsx
@@ -1,16 +1,17 @@
 import * as S from "./BestItems.styles";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import ItemCard from "../ItemCard/ItemCard";
 import { getProducts } from "../../../api/products";
+import useResize from "../../../hooks/useResize";
 
 export default function BestItems() {
   const [bestItems, setBestItems] = useState([]);
-  const [showItems, setShowItems] = useState(4);
+  const { showItems } = useResize(1, 2, 4);
 
   useEffect(() => {
     getProducts({
       page: 1,
-      pageSize: showItems,
+      pageSize: 4,
       orderBy: "favorite",
       keyword: "",
     }).then((result) => {
@@ -18,35 +19,16 @@ export default function BestItems() {
       const sortedBestItems = [...result.list].slice(0, 4);
       setBestItems(sortedBestItems);
     });
-  }, [showItems]);
-
-  const updateBestItems = useCallback(() => {
-    if (window.innerWidth <= 767) {
-      setShowItems(1);
-    } else if (window.innerWidth >= 768 && window.innerWidth <= 1199) {
-      setShowItems(2);
-    } else {
-      setShowItems(4);
-    }
   }, []);
 
   const responsiveItems = bestItems.slice(0, showItems);
-
-  useEffect(() => {
-    updateBestItems();
-    window.addEventListener("resize", updateBestItems);
-
-    return () => {
-      window.removeEventListener("resize", updateBestItems);
-    };
-  }, [updateBestItems]);
 
   return (
     <S.BestContainer>
       <S.Title>베스트 상품</S.Title>
       <S.ItemCardContainer>
-        {responsiveItems.map((items, idx) => (
-          <ItemCard key={idx} list="best" {...items} />
+        {responsiveItems.map((items) => (
+          <ItemCard key={items.id} list="best" {...items} />
         ))}
       </S.ItemCardContainer>
     </S.BestContainer>
