f09ee29ee998a80ae6c37a1580eb38f86a855582
heejin <heejin0857@naver.com>
2025-02-14 00:22:55 +0900

    Feat : comment input component

---
+++ src/components/Comment/CommentInput.styles.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+export const CommentContainer = styled.div`
+  width: 100%;
+`;
+
+export const RegisterBtn = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+
+  button {
+    padding: 12px 23px;
+    border-radius: 8px;
+    font: ${theme.font.H5Bold};
+    color: ${theme.color.gray100};
+    background-color: ${theme.color.blue};
+    margin-top: 24px;
+
+    &:disabled {
+      background-color: ${theme.color.gray400};
+    }
+
+    &:hover {
+      background-color: ${theme.color.blueHover};
+    }
+  }
+`;
