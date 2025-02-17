c7886fd26e9ac14957dbdb5080f6d73407012a97
Seok-jun.Kang <kangsj0327@gmail.com>
2025-02-17 18:02:59 +0900

    fix: Home 페이지 - FeatureCard 렌더링 시 key 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,6 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import FeatureCard from "@/components/FeatureCard";
 import PrimaryCard from "@/components/PrimaryCard";
-import { useNavigate } from "react-router";
 
 const PRIMARY_CARDS = [
   {
@@ -24,22 +23,23 @@ const PRIMARY_CARDS = [
 
 const FEATURE_CARDS = [
   {
+    id: 1,
     image: img_home_item,
     tag: "Hot item",
     title: "인기 상품을 확인해 보세요",
     content: "가장 HOT한 중고거래 물품을 판다 마켓에서 확인해 보세요",
     isReversed: false,
   },
-
   {
+    id: 2,
     image: img_home_search,
     tag: "Search",
     title: "구매를 원하는 상품을 검색하세요",
     content: "구매하고 싶은 물품은 검색해서 쉽게 찾아보세요",
     isReversed: true,
   },
-
   {
+    id: 3,
     image: img_home_register,
     tag: "Register",
     title: "판매를 원하는 상품을 등록하세요",
@@ -57,7 +57,7 @@ function Home() {
       </div>
       <div className="flex flex-col items-center pb-34.5">
         {FEATURE_CARDS.map((card) => (
-          <div className="p-34.5">
+          <div key={card.id} className="p-34.5">
             <FeatureCard {...card} />
           </div>
         ))}
