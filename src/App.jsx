f0d005ba87cf433e58a3387453f848359592c897
Seok-jun.Kang <kangsj0327@gmail.com>
2025-02-17 17:53:00 +0900

    chore: '/' 페이지 라우트 Home 페이지로 변경.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { StrictMode } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage.jsx";
-import LoginPage from "./pages/login.jsx";
-import SignupPage from "./pages/SIgnUpPage.jsx";
-import ItemsPage from "./pages/ItemsPage.jsx";
-import PrivacyPage from "./pages/PrivacyPage.jsx";
+import "@/App.css";
+import Home from "@/pages/Home.jsx";
+import LoginPage from "@/pages/login.jsx";
+import SignupPage from "@/pages/SIgnUpPage.jsx";
+import ItemsPage from "@/pages/ItemsPage.jsx";
+import PrivacyPage from "@/pages/PrivacyPage.jsx";
+import FAQ from "@/pages/FAQ.jsx";
 
 function App() {
   return (
     <StrictMode>
       <BrowserRouter>
-        {/* <App></App> */}
         <Routes>
-          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/signup" element={<SignupPage />}></Route>
           <Route path="/items" element={<ItemsPage />}></Route>
           <Route path="/privacy" element={<PrivacyPage />}></Route>
+          <Route path="/faq" element={<FAQ />}></Route>
         </Routes>
       </BrowserRouter>
     </StrictMode>
