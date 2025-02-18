7fa8d5fd38f53300c6c9fea025657ec0c740124d
Seok-jun.Kang <kangsj0327@gmail.com>
2025-02-19 00:04:37 +0900

    feat: FeatureCard 컴포넌트 반응형 레이아웃 구현

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,17 +1,21 @@
 function FeatureCard({ image, tag, title, content, isReversed }) {
   return (
     <div
-      className={`flex w-247 items-center justify-between gap-16 ${
-        isReversed ? "flex-row-reverse" : "flex-row"
+      className={`flex w-screen max-w-247 flex-col justify-between gap-6 px-4 break-keep md:px-6 xl:items-center xl:gap-16 xl:px-0 ${
+        isReversed ? "xl:flex-row-reverse" : "xl:flex-row"
       }`}
     >
-      <img src={image} alt="" />
+      <img className="md:w-auto" src={image} alt="" />
       <div
         className={`flex flex-col ${isReversed ? "items-end text-right" : "items-start"}`}
       >
-        <div className="text-blue-500">{tag}</div>
-        <div className="pt-3 text-5xl font-bold text-gray-700">{title}</div>
-        <div className="pt-6 text-gray-700">{content}</div>
+        <div className="font-bold text-blue-500 md:text-lg">{tag}</div>
+        <div className="mt-2 text-2xl font-bold text-gray-700 md:mt-4 md:text-3xl xl:w-72 xl:text-[40px]">
+          {title}
+        </div>
+        <div className="mt-4 text-lg whitespace-pre-line text-gray-700 md:mt-6 xl:text-2xl">
+          {content}
+        </div>
       </div>
     </div>
   );
