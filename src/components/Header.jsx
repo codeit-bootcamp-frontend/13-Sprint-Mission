f33d097f7e6dc6526f1dcf6dace77811dceecbc8
Seok-jun.Kang <kangsj0327@gmail.com>
2025-02-18 17:04:46 +0900

    feat: Header 컴포넌트  반응형 레이아웃 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import logoImage from "@/assets/logo_sm.svg";
+import logo_sm from "@/assets/logo_sm.svg";
+import logo from "@/assets/logo.svg";
 import { Link, useNavigate } from "react-router";
 
 function Header() {
@@ -8,19 +9,28 @@ function Header() {
   };
 
   return (
-    <header className="sticky top-0 flex h-17.5 w-full items-center justify-center border-b border-gray-200 bg-white">
-      <div className="flex w-480 items-center justify-between px-50">
+    <header className="sticky top-0 flex h-17.5 w-full items-center border-b border-gray-200 bg-white px-4 md:px-6 xl:px-50">
+      <div className="flex w-full items-center justify-between xl:max-w-480">
         <Link to="/" className="header__button">
-          <img src={logoImage} alt="홈으로 이동" />
+          <LogoImage />
         </Link>
         <button
           onClick={navigateToLogin}
-          className="h-12 w-32 rounded-lg bg-blue-500 text-gray-100"
+          className="h-12 w-32 cursor-pointer rounded-lg bg-blue-500 font-semibold text-gray-100"
         >
           로그인
         </button>
       </div>
     </header>
+  );
+}
+
+function LogoImage() {
+  return (
+    <>
+      <img src={logo} alt="홈으로 이동" className="block md:hidden" />
+      <img src={logo_sm} alt="홈으로 이동" className="hidden md:block" />
+    </>
   );
 }
 
