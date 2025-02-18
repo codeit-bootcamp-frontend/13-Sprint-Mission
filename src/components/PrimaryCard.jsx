8e5e87a052194f6068a9335b2dee20dfcb85dd5e
Seok-jun.Kang <kangsj0327@gmail.com>
2025-02-18 23:30:07 +0900

    feat: PrimaryCard 컴포넌트 반응형 UI 구현

--- src/components/PrimaryCard.jsx
+++ src/components/PrimaryCard.jsx
@@ -7,19 +7,21 @@ function PrimaryCard({ image, title, isButtonVisible }) {
   };
 
   return (
-    <div className="flex w-277.5 items-center justify-between">
-      <div className="pb-15">
-        {title}
+    <div className="flex w-277.5 flex-col items-center justify-between xl:flex-row">
+      <div className="mt-12 flex flex-col items-center justify-center xl:mt-0 xl:items-start">
+        <div className="w-60 text-center text-[32px]/11 font-bold break-keep text-gray-700 md:w-full md:text-[40px]/14 xl:w-74 xl:text-left">
+          {title}
+        </div>
         {isButtonVisible && (
           <button
-            className="mt-8 h-14 rounded-[40px] bg-blue-500 px-31 py-3 text-gray-50"
+            className="mt-4 h-12 cursor-pointer rounded-[40px] bg-blue-500 px-18 py-3 text-lg font-semibold text-gray-50 md:mt-8 md:h-14 md:px-31 md:text-xl"
             onClick={navigateToItems}
           >
             구경하러 가기
           </button>
         )}
       </div>
-      <img src={image} className="max-w-400" />
+      <img src={image} className="mt-8 xl:mt-0 xl:max-w-400" />
     </div>
   );
 }
