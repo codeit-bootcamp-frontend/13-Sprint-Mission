d40c15c6ef8fad822bb8d5465e65e467fc1f16af
hyeonjiroh <sonamu6509@naver.com>
2025-02-19 12:11:14 +0900

    feat: allow only one image selection and display image preview

--- React/panda-market/src/pages/RegisterItemPage/RegisterItemPage.jsx
+++ React/panda-market/src/pages/RegisterItemPage/RegisterItemPage.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import PrusIcon from "../../assets/icon/ic_plus.svg";
 import DeleteIcon from "../../assets/icon/ic_delete.svg";
 import styles from "./RegisterItemPage.module.css";
 
@@ -11,9 +12,33 @@ function RegisterItemPage() {
     tag: "",
   });
   const [tagValues, setTagValues] = useState([]);
+  const [itemImg, setItemImg] = useState(null);
+  const [fileError, setFileError] = useState("");
+  const fileInputRef = useRef(null);
 
   const handleRegister = async (event) => {
     event.preventDefault();
+  };
+
+  const handleFileButtonClick = () => {
+    if (!fileInputRef.current) return;
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+
+    if (!files) {
+      return;
+    }
+
+    if (files.length > 1) {
+      setFileError("*이미지 등록은 최대 1개까지 가능합니다.");
+      return;
+    }
+
+    setFileError("");
+    setItemImg(URL.createObjectURL(files[0]));
   };
 
   const handleChange = (event) => {
@@ -62,6 +87,34 @@ function RegisterItemPage() {
       <div className={styles.itemFormSection}>
         <div className={styles.infoInputSection}>
           <h2 className={styles.title}>상품 이미지</h2>
+          <div className={styles.imgSectionContainer}>
+            <button
+              type="button"
+              className={styles.imgAddButton}
+              onClick={handleFileButtonClick}
+            >
+              <input
+                type="file"
+                ref={fileInputRef}
+                style={{ display: "none" }}
+                onChange={handleFileChange}
+                accept=".jpeg, .jpg, .png"
+                multiple
+              />
+              <div className={styles.imgAddButtonContent}>
+                <div>
+                  <img src={PrusIcon} alt="이미지 등록" />
+                </div>
+                <div className={styles.imgAddButtonText}>이미지 등록</div>
+              </div>
+            </button>
+            {itemImg && (
+              <div className={styles.imgPreview}>
+                <img src={itemImg} alt="상품 이미지 미리보기" />
+              </div>
+            )}
+          </div>
+          {fileError && <div className={styles.errorMessage}>{fileError}</div>}
         </div>
         <div className={styles.infoInputSection}>
           <h2 className={styles.title}>상품명</h2>
