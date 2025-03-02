aded473fa5b2cfd2001b4f08be45ba16a0a030f6
gyoyeon Kim <gy2121@naver.com>
2025-03-02 20:34:54 +0900

    feat: 로그인 - 눈 모양 아이콘

--- Login/login.js
+++ Login/login.js
@@ -4,12 +4,26 @@ document.addEventListener("DOMContentLoaded", function () {
   const passwordInput = document.getElementById("password");
   const passwordError = document.getElementById("password_error");
   const loginButton = document.querySelector(".btn_login2");
+  const togglePasswordIcon = document.querySelector(".input_icon");
 
+  /* 비밀번호 보이기 */
+  togglePasswordIcon.addEventListener("click", function () {
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text"; // 비밀번호 보이기
+      togglePasswordIcon.src = "../img/login/btn_hidepw.svg";
+    } else {
+      passwordInput.type = "password"; // 비밀번호 가리기
+      togglePasswordIcon.src = "../img/login/btn_showpw.svg";
+    }
+  });
+
+  /* 이메일 유효성 */
   function validateEmail(email) {
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
   }
 
+  /* 비밀번호 유효성 */
   function validateInputs() {
     const isEmailValid = emailInput.value && validateEmail(emailInput.value);
     const isPasswordValid =
@@ -25,6 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  /* 이메일 에러메세지 */
   function checkEmail() {
     if (!validateEmail(emailInput.value)) {
       emailError.textContent = "잘못된 이메일 형식입니다.";
@@ -37,6 +52,7 @@ document.addEventListener("DOMContentLoaded", function () {
     validateInputs();
   }
 
+  /* 비밀번호 에러메세지 */
   function checkPassword() {
     if (passwordInput.value.length < 8) {
       passwordError.textContent = "비밀번호를 8자 이상 입력해주세요.";
@@ -47,18 +63,15 @@ document.addEventListener("DOMContentLoaded", function () {
     validateInputs();
   }
 
-  emailInput.addEventListener("blur", checkEmail);
-  passwordInput.addEventListener("blur", checkPassword);
-
-  // 🎯 실시간 입력 감지 (input 이벤트 추가)
   emailInput.addEventListener("input", checkEmail);
   passwordInput.addEventListener("input", checkPassword);
 
+  /* 로그인 버튼 활성화 */
   loginButton.addEventListener("click", function (event) {
     if (!loginButton.classList.contains("active")) {
-      event.preventDefault(); // 비활성화 상태에서 클릭 방지
+      event.preventDefault(); // 비활성화 상태 클릭 방지
     } else {
-      window.location.href = "/items"; // /items 페이지로 이동
+      window.location.href = "/items";
     }
   });
 
