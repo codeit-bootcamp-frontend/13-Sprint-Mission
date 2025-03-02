02ee1c6d25709e14386890d5ff36c9959b02aff6
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-02 19:51:32 +0900

    feat: postItem 함수 구현

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,10 +1,11 @@
+const BASE_URL = "https://panda-market-api.vercel.app/products/";
+
 export const getItems = async (
   page,
   pageSize,
   orderBy = "recent",
   keyword = null,
 ) => {
-  const baseURL = "https://panda-market-api.vercel.app/products/";
   const params = new URLSearchParams({
     page: page,
     pageSize: pageSize,
@@ -15,10 +16,22 @@ export const getItems = async (
     params.append("keyword", keyword);
   }
 
-  const requrl = `${baseURL}?${params.toString()}`;
+  const requrl = `${BASE_URL}?${params.toString()}`;
 
   const response = await fetch(requrl, {
     method: "GET",
+  });
+  const data = await response.json();
+  return data;
+};
+
+export const postItem = async (item) => {
+  const response = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(item),
   });
   const data = await response.json();
   return data;
