e96925781d1c0386b0e8a9430d033380e675e98b
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-02 21:54:12 +0900

    feat: AddItem 페이지 - TextField 컴포넌트 사용

--- src/pages/AddItem.jsx
+++ src/pages/AddItem.jsx
@@ -4,6 +4,7 @@ import { ITEM_REGISTRATION } from "@/constants/formFields";
 import { useState } from "react";
 import ic_plus from "@/assets/ic_plus.svg";
 import ic_close from "@/assets/ic_close.svg";
+import TextField from "@/components/TextField";
 
 const item = {
   images: [],
@@ -41,68 +42,85 @@ function AddItem() {
               등록
             </button>
           </div>
-          <div>
-            상품 이미지
-            {image === null ? (
-              <>
-                <label
-                  htmlFor="file-input"
-                  className="flex size-42 cursor-pointer flex-col items-center justify-center gap-2.5 rounded-xl bg-gray-100 font-normal text-gray-400"
-                >
-                  <img src={ic_plus} className="size-12" />
-                  이미지 업로드
-                </label>
-                <input
-                  id="file-input"
-                  type="file"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                  className="hidden"
-                />
-              </>
-            ) : (
-              <div className="relative size-42">
-                <button type="button" className={`absolute top-3 right-3`}>
-                  <img
-                    src={ic_close}
-                    alt="X"
-                    className="cursor-pointer"
-                    onClick={() => {
-                      setImage(null);
-                    }}
-                  />
-                </button>
-                <img
-                  src={URL.createObjectURL(image)}
-                  alt="미리보기"
-                  className="aspect-square rounded-xl object-cover"
-                />
+          <div className="flex flex-col gap-6">
+            <div>
+              <div className="mb-4 text-lg font-bold text-gray-800">
+                상품 이미지
               </div>
+              {image === null ? (
+                <>
+                  <label
+                    htmlFor="file-input"
+                    className="flex size-42 cursor-pointer flex-col items-center justify-center gap-2.5 rounded-xl bg-gray-100 font-normal text-gray-400"
+                  >
+                    <img src={ic_plus} className="size-12" />
+                    이미지 업로드
+                  </label>
+                  <input
+                    id="file-input"
+                    type="file"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    className="hidden"
+                  />
+                </>
+              ) : (
+                <div className="relative size-42">
+                  <button type="button" className={`absolute top-3 right-3`}>
+                    <img
+                      src={ic_close}
+                      alt="X"
+                      className="cursor-pointer"
+                      onClick={() => {
+                        setImage(null);
+                      }}
+                    />
+                  </button>
+                  <img
+                    src={URL.createObjectURL(image)}
+                    alt="미리보기"
+                    className="aspect-square rounded-xl object-cover"
+                  />
+                </div>
+              )}
+            </div>
+            {Object.entries(ITEM_REGISTRATION).map(([key, value]) =>
+              key === "explanation" ? (
+                <TextField
+                  key={key}
+                  name={value.id}
+                  // onChange={handleFieldChange}
+                  // onBlur={handleFieldBlur}
+                  // hasValue={isFieldFilled[name]}
+                  // isValidated={isFieldValidated[name]}
+                  // value={formData[name]}
+                  {...value}
+                />
+              ) : (
+                <InputField
+                  key={key}
+                  name={value.id}
+                  // onChange={handleFieldChange}
+                  // onBlur={handleFieldBlur}
+                  // hasValue={isFieldFilled[name]}
+                  // isValidated={isFieldValidated[name]}
+                  // value={formData[name]}
+                  {...value}
+                />
+              ),
             )}
+            <div className="-mt-3 flex justify-start gap-3">
+              {tags.map((tag) => (
+                <div className="flex gap-2 rounded-full bg-gray-100 py-1.5 pr-3 pl-4 font-normal">
+                  <div>{`#${tag}`}</div>
+                  <button className="cursor-pointer">
+                    <img src={ic_close} alt="" />
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
         </form>
-        {Object.entries(ITEM_REGISTRATION).map(([key, value]) => (
-          <InputField
-            key={key}
-            name={value.id}
-            // onChange={handleFieldChange}
-            // onBlur={handleFieldBlur}
-            // hasValue={isFieldFilled[name]}
-            // isValidated={isFieldValidated[name]}
-            // value={formData[name]}
-            {...value}
-          />
-        ))}
-        <div className="mt-3 flex justify-start gap-3">
-          {tags.map((tag) => (
-            <div className="flex gap-2 rounded-full bg-gray-100 py-1.5 pr-3 pl-4 font-normal">
-              <div>{`#${tag}`}</div>
-              <button className="cursor-pointer">
-                <img src={ic_close} alt="" />
-              </button>
-            </div>
-          ))}
-        </div>
       </main>
     </>
   );
