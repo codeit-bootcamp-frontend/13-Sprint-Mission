2d8f1785ec88df5c50fb4b2ff95f24a7410d4f25
Seok-jun.Kang <kangsj0327@gmail.com>
2025-03-02 17:51:01 +0900

    feat: Spinner 컴포넌트 생성

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+function Spinner() {
+  return (
+    <div className="m-auto h-12 w-12 animate-spin rounded-full border-5 border-solid border-blue-500 border-b-transparent"></div>
+  );
+}
+
+export default Spinner;
