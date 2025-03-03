213d5b6f16265ba96d6899548dbeb3e8d8ae7fce
gyoyeon Kim <gy2121@naver.com>
2025-03-03 16:00:14 +0900

    로그인- 이메일 에러 메세지 추가

--- Login/login.js
+++ Login/login.js
@@ -43,7 +43,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
   /* 이메일 에러메세지 */
   function checkEmail() {
-    if (!validateEmail(emailInput.value)) {
+    if (!emailInput.value) {
+      emailError.textContent = "이메일을 입력해주세요.";
+      emailError.style.display = "block";
+      emailInputBorder.classList.add("error-border");
+    } else if (!validateEmail(emailInput.value)) {
       emailError.textContent = "잘못된 이메일 형식입니다.";
       emailError.style.display = "block";
       emailInputBorder.classList.add("error-border"); // input 오류
