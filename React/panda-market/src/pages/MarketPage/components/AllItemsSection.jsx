b593f18905a2fa11dd7ea2eea02170e39f507fe8
hyeonjiroh <sonamu6509@naver.com>
2025-03-07 15:05:20 +0900

    fix: Fix pagination button styling after page 5

--- React/panda-market/src/pages/MarketPage/components/AllItemsSection.jsx
+++ React/panda-market/src/pages/MarketPage/components/AllItemsSection.jsx
@@ -167,7 +167,9 @@ function AllItems() {
         {PAGE_ARRAY.map((num) => (
           <button
             className={`${styles.pageButton} ${
-              num === page ? styles.activePage : ""
+              Number(num + PAGE_CHUNK_SIZE * pageBound) === page
+                ? styles.activePage
+                : ""
             }`}
             value={num + PAGE_CHUNK_SIZE * pageBound}
             onClick={changePage}
