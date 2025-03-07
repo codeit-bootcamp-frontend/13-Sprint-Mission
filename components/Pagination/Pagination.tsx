7cc5f45f056dc99b1038c5fbf970c91b955d857e
heejin <heejin0857@naver.com>
2025-03-08 02:29:38 +0900

    Style : pagination tailwind css

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import * as S from "./Pagination.styles";
 import left from "@/public/icons/arrowLeft.svg";
 import right from "@/public/icons/arrowRight.svg";
 import usePagination from "./usePagination";
+import Link from "next/link";
+import Image from "next/image";
 
 export interface PagingProps {
   totalBoards: number;
@@ -23,49 +24,52 @@ export default function Pagination({
   });
 
   return (
-    <S.Pages>
-      <S.LinkBtn
+    <div className="w-full flex justify-center items-center gap-1">
+      <Link
+        className="w-10 h-10 flex justify-center items-center rounded-[40px]border-b border-gray200 bg-white cursor-pointer"
         href={currentPage > 1 ? createPageParams(currentPage - 1) : "#"}
         shallow
         scroll={false}
       >
-        <S.Arrow
-          disabled={currentPage === 1}
+        <Image
+          className={`${currentPage === 1 ? 'opacity-20' : 'opacity-100'}`}
           src={left}
           width={16}
           height={16}
           alt="prev"
         />
-      </S.LinkBtn>
+      </Link>
       {Array.from(
         { length: endPage - startPage + 1 },
         (_, i) => startPage + i
       ).map((page) => (
-        <S.Page
+        <Link
+          className={`w-10 h-10 flex justify-center items-center text-Bold16 rounded-[40px] border-b border-gray200 cursor-pointer
+          ${currentPage === page ? 'text-gray50 bg-blue' : 'text-gray500 bg-white'}`}        
           key={page}
-          $isActive={currentPage === page}
           href={currentPage !== page ? createPageParams(page) : "#"}
           shallow
           scroll={false}
         >
           {page}
-        </S.Page>
+        </Link>
       ))}
-      <S.LinkBtn
+      <Link
+        className="w-10 h-10 flex justify-center items-center rounded-[40px]border-b border-gray200 bg-white cursor-pointer"
         href={
           currentPage < totalPages ? createPageParams(currentPage + 1) : "#"
         }
         shallow
         scroll={false}
       >
-        <S.Arrow
-          disabled={currentPage === totalBoards}
+        <Image
+          className={`${currentPage === totalBoards ? 'opacity-20' : 'opacity-100'}`}
           src={right}
           width={16}
           height={16}
           alt="next"
         />
-      </S.LinkBtn>
-    </S.Pages>
+      </Link>
+    </div>
   );
 }
