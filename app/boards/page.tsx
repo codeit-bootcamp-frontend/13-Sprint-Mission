371173ed06e9f2f5b01138beed21f6703dd2ee9c
heejin <heejin0857@naver.com>
2025-03-08 02:27:20 +0900

    Style : boards page tailwindcss

--- app/boards/page.tsx
+++ app/boards/page.tsx
@@ -1,32 +1,13 @@
-import * as S from "./Boards.styles";
 import Best from "@/components/BestBoards/Best";
 import All from "@/components/AllBoards/All";
-import { getBoards } from "@/apis/boards";
 
 export default async function Boards() {
-  const allData = await getBoards({
-    page: 1,
-    pageSize: 10,
-    orderBy: "recent",
-    keyword: "",
-  });
-  const list = allData.list;
-  const totalCount = allData.totalCount;
-
-  const bestData = await getBoards({
-    page: 1,
-    pageSize: 3,
-    orderBy: "like",
-    keyword: "",
-  });
-  const best = bestData.list;
-
   return (
-    <S.Container>
-      <S.Board>
-        <Best best={best} />
-        <All list={list} totalCount={totalCount} />
-      </S.Board>
-    </S.Container>
+    <div className="flex justify-center items-center my-10">
+      <div className="flex flex-col gap-10">
+        <Best />
+        <All />
+      </div>
+    </div>
   );
 }
