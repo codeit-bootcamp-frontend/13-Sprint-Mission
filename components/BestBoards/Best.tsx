7efcd7fe29d4f71ec2612f7119d6df6b5b8acb89
heejin <heejin0857@naver.com>
2025-03-10 03:56:13 +0900

    Feat - useBestData - useResize custom hook - tailwind css edit

--- components/BestBoards/Best.tsx
+++ components/BestBoards/Best.tsx
@@ -1,18 +1,13 @@
-import { getBoards } from "@/apis/boards";
+"use client";
+
 import BestItem from "./BestItem";
+import useBestData from "./useBestData";
 
-export default async function Best() {
-  const bestData = await getBoards({
-    page: 1,
-    pageSize: 3,
-    orderBy: "like",
-    keyword: "",
-  });
-
-  const best = bestData.list;
+export default function Best() {
+  const { best } = useBestData();
 
   return (
-    <div className="flex flex-col items-start gap-6">
+    <div className="w-full flex flex-col items-start gap-6">
       <h2 className="text-gray900 text-Bold20 font-bold">베스트 게시글</h2>
       <div className="flex justify-start items-center gap-6">
         {best.map((item) => (
--- components/BestBoards/BestItem.tsx
+++ components/BestBoards/BestItem.tsx
@@ -11,27 +11,40 @@ interface BestItemProps {
 
 export default function BestItem({ best }: BestItemProps) {
   return (
-    <Link className="w-[384px] h-[169px] px-6 bg-gray50 rounded-lg cursor-pointer" href="/">
-      <div className="flex flex-col justify-center items-start gap-3">
+    <Link
+      className="w-full h-[169px] px-6 bg-gray50 rounded-lg cursor-pointer md:h-[198px] maxM:w-full maxM:h-[198px]"
+      href="/"
+    >
+      <div className="w-full h-full flex flex-col justify-start items-start gap-3 maxM:gap-4">
         <div className="w-[102px] h-[30px] flex justify-center items-center gap-1 bg-blue rounded-bl-[16px] rounded-br-[16px]">
           <Image src={medal} width={16} height={16} alt="medal" />
           <span className="text-white text-Bold16">Best</span>
         </div>
-        <div className="w-full flex justify-between items-center gap-2">
-          <p className="line-break flex-1 h-[60px] break-words text-gray800 text-Bold20">{best.content}</p>
-          <div className="relative w-[72px] h-[72px] bg-white border-b border-gray200 rounded-md">
-            <Image fill src={best.image} alt="image" />
-          </div>
-        </div>
-        <div className="w-full flex justify-between items-center">
-          <div className="flex justify-center items-end gap-2">
-            <span className="text-gray600 text-Regular14">{best.writer.nickname}</span>
-            <div className="flex justify-center items-center gap-1">
-              <Image src={heart} width={16} height={16} alt="like" />
-              <span className="text-gray500 text-Regular14">{best.likeCount}</span>
+        <div className="flex flex-col gap-3 md:gap-10 maxM:gap-10">
+          <div className="w-full flex justify-between items-center gap-2">
+            <p className="line-break flex-1 h-[60px] break-words text-gray800 text-Bold20">
+              {best.content}
+            </p>
+            <div className="relative w-[72px] h-[72px] bg-white border-b border-gray200 rounded-md">
+              <Image fill src={best.image} alt="image" />
             </div>
           </div>
-          <div className="text-gray400 text-Regular14">{formattedDate(best.createdAt)}</div>
+          <div className="w-full flex justify-between items-center">
+            <div className="flex justify-center items-end gap-2">
+              <span className="text-gray600 text-Regular14">
+                {best.writer.nickname}
+              </span>
+              <div className="flex justify-center items-center gap-1">
+                <Image src={heart} width={16} height={16} alt="like" />
+                <span className="text-gray500 text-Regular14">
+                  {best.likeCount}
+                </span>
+              </div>
+            </div>
+            <div className="text-gray400 text-Regular14">
+              {formattedDate(best.createdAt)}
+            </div>
+          </div>
         </div>
       </div>
     </Link>
