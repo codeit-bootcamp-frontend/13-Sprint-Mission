032c9678c9063fb7b557f05bb8f2b022c96fe28a
heejin <heejin0857@naver.com>
2025-03-10 03:57:52 +0900

    Style : boards page tailwind css edit

--- app/boards/page.tsx
+++ app/boards/page.tsx
@@ -1,10 +1,10 @@
 import Best from "@/components/BestBoards/Best";
 import All from "@/components/AllBoards/All";
 
-export default async function Boards() {
+export default function Boards() {
   return (
-    <div className="flex justify-center items-center my-10">
-      <div className="flex flex-col gap-10">
+    <div className="flex justify-center items-center my-10 lg:my-0">
+      <div className="max-w-[1200px] min-w-[343px] px-6 lg:p-4 md:p-6 flex flex-col gap-10">
         <Best />
         <All />
       </div>
