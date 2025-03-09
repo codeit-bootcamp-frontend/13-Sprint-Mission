a9749a6c44fd3da71aa8a4e8b4ab725d06544fb4
heejin <heejin0857@naver.com>
2025-03-10 03:55:08 +0900

    Style : tailwind css edit

--- components/common/Dropdown/Dropdown.tsx
+++ components/common/Dropdown/Dropdown.tsx
@@ -24,18 +24,25 @@ export default function Dropdown({
   };
 
   return (
-    <div className="relative w-[130px] h-[42px] shrink-0 flex flex-col items-end gap-5">
-      <div className="w-full h-[42px] flex justify-between items-center py-3 px-5 rounded-xl border border-solid border-gray200 bg-white cursor-pointer" onClick={handleOpenClick}>
+    <div className="relative w-[130px] h-[42px] shrink-0 flex flex-col items-end gap-5 maxS:w-[42px]">
+      <div
+        className="w-full h-[42px] flex justify-between items-center py-3 px-5 rounded-xl border border-solid border-gray200 bg-white cursor-pointer maxS:hidden"
+        onClick={handleOpenClick}
+      >
         <span className="">{orderBy}</span>
         <Image src={isOpen ? up : down} width={24} height={24} alt="arrow" />
       </div>
-      <div className="w-[42px] h-[42px] shrink-0 hidden justify-center items-center rounded-xl border border-solid border-gray200 bg-white cursor-pointer md:flex" onClick={handleOpenClick}>
+      <div
+        className="w-[42px] h-[42px] shrink-0 hidden justify-center items-center rounded-xl border border-solid border-gray200 bg-white cursor-pointer maxS:flex"
+        onClick={handleOpenClick}
+      >
         <Image src={dropdown} width={24} height={24} alt="dropdown" />
       </div>
       {isOpen && (
         <div className="absolute top-14 z-[99] w-[130px] bg-white border border-solid border-gray200 rounded-xl text-gray800 ">
           {list.map((item) => (
-            <div className="h-[42px] flex justify-center items-center border-b border-gray200 text-Regular16 cursor-pointer hover:text-blue last:border-none"
+            <div
+              className="h-[42px] flex justify-center items-center border-b border-gray200 text-Regular16 cursor-pointer hover:text-blue last:border-none"
               key={item}
               onClick={() => {
                 onChange(item);
