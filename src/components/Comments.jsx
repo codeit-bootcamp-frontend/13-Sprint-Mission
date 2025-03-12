f673cb66324384fe391f0e5d745ef21a3bb11c24
hyejin <hhyejin90@gmail.com>
2025-03-12 15:28:16 +0900

    [Refector] 스타일 분리

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,29 +1,28 @@
-import styled from "styled-components";
-import theme from "../styles/theme";
+import * as S from "./Comments.styles";
 import ic_user from "../assets/images/icons/ic_user.svg";
 import EditDropdown from "./common/EditDropdown";
 
 export default function Comments({ comments }) {
   return (
-    <CommentContainer>
+    <S.CommentContainer>
       {comments.map((comment) => (
-        <InquiryItem key={comment.id}>
-          <CommentHeader>
-            <UserInfo>
-              <ProfileImage src={ic_user} alt="프로필" />
-              <UserDetails>
-                <Nickname>{comment.writer?.nickname || "알 수 없음"}</Nickname>
-                <TimeAgo>{getTimeAgo(comment.createdAt)}</TimeAgo>
-              </UserDetails>
-            </UserInfo>
-
+        <S.InquiryItem key={comment.id}>
+          <S.CommentHeader>
+            <S.CommentText>{comment.content}</S.CommentText>
             <EditDropdown />
-          </CommentHeader>
-
-          <CommentText>{comment.content}</CommentText>
-        </InquiryItem>
+          </S.CommentHeader>
+          <S.UserInfo>
+            <S.ProfileImage src={ic_user} alt="프로필" />
+            <S.UserDetails>
+              <S.Nickname>
+                {comment.writer?.nickname || "알 수 없음"}
+              </S.Nickname>
+              <S.TimeAgo>{getTimeAgo(comment.createdAt)}</S.TimeAgo>
+            </S.UserDetails>
+          </S.UserInfo>
+        </S.InquiryItem>
       ))}
-    </CommentContainer>
+    </S.CommentContainer>
   );
 }
 
@@ -44,55 +43,3 @@ function getTimeAgo(createdAt) {
     return `${diffInDays}일 전`; // 1일 이상이면 "n일 전"
   }
 }
-
-const CommentContainer = styled.div`
-  margin-top: 16px;
-`;
-
-const InquiryItem = styled.div`
-  padding: 10px;
-  border-bottom: 1px solid ${theme.colors.Gray200};
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-`;
-
-const CommentHeader = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-`;
-
-const CommentText = styled.p`
-  font: ${theme.fonts.H6Regular};
-  color: ${theme.colors.Gray800};
-`;
-
-const UserInfo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 12px;
-`;
-
-const ProfileImage = styled.img`
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
-`;
-
-const UserDetails = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const Nickname = styled.span`
-  font: ${theme.fonts.H8};
-  color: ${theme.colors.Gray600};
-`;
-
-const TimeAgo = styled.span`
-  font-size: 12px;
-  color: ${theme.colors.Gray400};
-  margin-top: 4px;
-`;
