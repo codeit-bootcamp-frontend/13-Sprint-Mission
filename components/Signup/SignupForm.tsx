47de8ecdb6f067902dafceef54708fae69ada03b
heejin <heejin0857@naver.com>
2025-04-07 23:42:56 +0900

    Feat : signup form logic

--- components/Signup/SignupForm.tsx
+++ components/Signup/SignupForm.tsx
@@ -4,12 +4,13 @@ import { useState } from "react";
 import Button from "../common/Button/Button";
 import FormField from "../common/FormField/FormField";
 import PasswordToggleBtn from "../PasswordToggleBtn/PasswordToggleBtn";
+import useSignup from "./useSignup";
 
 const SIGNUP_FORM_FIELDS = [
   {
     label: "이메일",
     name: "email",
-    type: "email",
+    type: "text",
     placeholder: "이메일을 입력해주세요",
   },
   {
@@ -30,30 +31,28 @@ const SIGNUP_FORM_FIELDS = [
     type: "password",
     placeholder: "비밀번호를 다시 한 번 입력해주세요",
   },
-];
+] as const;
 
 export default function SignupForm() {
-  const [isPasswordVisible, setIsPasswordVisible] = useState<
-    Record<string, boolean>
-  >({
-    password: false,
-    checkPassword: false,
-  });
-
-  const toggleVisiblePassword = (name: string) => {
-    setIsPasswordVisible((prev) => ({
-      ...prev,
-      [name]: !prev[name],
-    }));
-  };
-
+  const {
+    formData,
+    state,
+    isPasswordVisible,
+    isPending,
+    isFormComplete,
+    handleFormChange,
+    handlePreventSpace,
+    toggleVisiblePassword,
+    handleSignupSubmit,
+  } = useSignup();
   return (
-    <form className="flex flex-col gap-6">
+    <form onSubmit={handleSignupSubmit} className="flex flex-col gap-6">
       {SIGNUP_FORM_FIELDS.map((field) => {
         const { label, name, type, placeholder } = field;
         const isPassword = isPasswordVisible[name];
         const isPasswordField = ["password", "checkPassword"].includes(name);
         const passwordType = isPassword ? "text" : "password";
+        const isInputFieldValid = state?.field === name;
 
         return (
           <FormField
@@ -61,6 +60,11 @@ export default function SignupForm() {
             label={label}
             name={name}
             type={isPassword ? passwordType : type}
+            value={formData[name]}
+            isValid={!isInputFieldValid}
+            errorMessage={state.message}
+            onChange={handleFormChange}
+            onKeyDown={handlePreventSpace}
             placeholder={placeholder}
             rightSlot={
               isPasswordField && (
@@ -73,8 +77,14 @@ export default function SignupForm() {
           />
         );
       })}
-      <Button fullWidth fontSize="20" paddingY={16}>
-        회원가입
+      <Button
+        disabled={!isFormComplete}
+        type="submit"
+        fullWidth
+        fontSize="20"
+        paddingY={16}
+      >
+        {isPending ? "..." : "회원가입"}
       </Button>
     </form>
   );
---
+++ components/Signup/useSignup.tsx
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import checkAllFormComplete from "@/utils/checkAllFormComplete";
+import axios from "axios";
+
+interface SignupType {
+  email: string;
+  nickname: string;
+  password: string;
+  checkPassword: string;
+}
+
+interface SignupState {
+  status: boolean;
+  field?: string;
+  message: string;
+}
+
+const INITIAL_SIGNUP_FORM_VALUE = {
+  email: "",
+  nickname: "",
+  password: "",
+  checkPassword: "",
+};
+
+const SPACE_KEY = " ";
+
+export default function useSignup() {
+  const [formData, setFormData] = useState<SignupType>(
+    INITIAL_SIGNUP_FORM_VALUE
+  );
+  const [isPasswordVisible, setIsPasswordVisible] = useState<
+    Record<string, boolean>
+  >({
+    password: false,
+    checkPassword: false,
+  });
+  const [state, setState] = useState<SignupState>({
+    status: false,
+    field: "",
+    message: "",
+  });
+  const [isPending, setIsPending] = useState(false);
+
+  const isFormComplete = checkAllFormComplete({
+    email: formData.email,
+    nickname: formData.nickname,
+    password: formData.password,
+    checkPassword: formData.checkPassword,
+  });
+
+  const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handlePreventSpace = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== SPACE_KEY) return;
+    e.preventDefault();
+  };
+
+  const toggleVisiblePassword = (name: string) => {
+    setIsPasswordVisible((prev) => ({
+      ...prev,
+      [name]: !prev[name],
+    }));
+  };
+
+  const handleSignupSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!isFormComplete) return;
+
+    setIsPending(true);
+
+    try {
+      const response = await fetch("/api/auth/signUp", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: formData.email,
+          nickname: formData.nickname,
+          password: formData.password,
+          passwordConfirmation: formData.checkPassword,
+        }),
+      });
+
+      const result = await response.json();
+
+      if (!response.ok) {
+        const { status, field, message } = result;
+        setState({ status, field, message });
+        return;
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  return {
+    formData,
+    state,
+    isPasswordVisible,
+    isPending,
+    isFormComplete,
+    handleFormChange,
+    handlePreventSpace,
+    toggleVisiblePassword,
+    handleSignupSubmit,
+  };
+}
