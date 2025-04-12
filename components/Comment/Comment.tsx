d043787c16be9bec1b75184c80f1c83091da9f33
heejin <heejin0857@naver.com>
2025-04-13 02:14:28 +0900

    Feat : get comments

--- components/Comment/Comment.tsx
+++ components/Comment/Comment.tsx
@@ -2,9 +2,9 @@ import Button from "../common/Button/Button";
 import Textarea from "../common/Textarea/Textarea";
 import CommentList from "./CommentList";
 
-export default function Comment() {
+export default function Comment({ articleId }: { articleId: number }) {
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-6 sm:gap-8">
       <div className="flex flex-col items-end gap-4">
         <div className="flex w-full flex-col gap-4">
           <label className="text-semi16 text-gray900">댓글달기</label>
@@ -14,7 +14,7 @@ export default function Comment() {
           등록
         </Button>
       </div>
-      <CommentList comment={[]} />
+      <CommentList articleId={articleId} />
     </div>
   );
 }
