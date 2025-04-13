a95cec7620b785fbc2cdcd98d7a4486d4d97b6f9
jjaneyxx <hmyeonge@gmail.com>
2025-04-13 14:37:59 +0900

    chore: add tailwind color utility class

--- panda-market/src/components/layout/Header.tsx
+++ panda-market/src/components/layout/Header.tsx
@@ -2,8 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
-  // 자유게시판 boards
-  // 중고마켓 items
   return (
     <header>
       <div className="flex justify-between items-center mx-auto px-[200px]">
@@ -11,7 +9,7 @@ export default function Header() {
           <Link href="/">
             <Image src="/icons/logo.svg" alt="logo" width={0} height={0} style={{ width: "153px", height: "auto" }} />
           </Link>
-          <div className="flex items-center gap-2.5  ml-8">
+          <div className="flex items-center gap-2.5 ml-8">
             <Link href="/boards" className="px-[15px] py-[21px]">
               자유게시판
             </Link>
