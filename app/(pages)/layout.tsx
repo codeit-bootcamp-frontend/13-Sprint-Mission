e1647560b9052d417162473e06e1643d45f73579
heejin <heejin0857@naver.com>
2025-04-17 04:05:40 +0900

    Fix : delete layout user zustand

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,18 +1,10 @@
 import Header from "@/components/common/Header/Header";
-import { getUserId } from "../lib/getUserId";
-import { useUserStore } from "@/store/useUserStore";
 
 export default async function PagesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const userId = await getUserId();
-
-  if (userId) {
-    useUserStore.getState().setUserId(userId);
-  }
-
   return (
     <div className="flex h-screen flex-col">
       <Header />
