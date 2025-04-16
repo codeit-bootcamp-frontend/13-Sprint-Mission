71baddd57693c7a933095dac98f1d54e0c8e28df
heejin <heejin0857@naver.com>
2025-04-17 04:11:55 +0900

    Feat : has accessToken redirect /

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,22 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
-import panda from "@/public/icons/panda.svg";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function AuthLayout({
+export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const cookie = await cookies();
+  const accessToken = cookie.get("accessToken")?.value;
+
+  if (accessToken) {
+    redirect("/");
+  }
+
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex w-[640px] flex-col items-center justify-center gap-10 px-4 sm:px-6">
         <Link className="flex items-center gap-5" href="/">
           <Image
-            src={panda}
+            src="/icons/panda.svg"
             width={103}
             height={103}
             alt="logo"
-            className="[@media(max-width:480px)]:hidden"
+            className="hidden sm:block"
           />
           <h1 className="font-rokaf text-blue text-[66px] font-bold">
             판다마켓
