89c0862243080d79565c03b636dafb9261e0260a
jjaneyxx <hmyeonge@gmail.com>
2025-04-20 19:32:21 +0900

    refactor: 드롭다운 컴포넌트 내부 선언 순서 개선
    - handleDropDownToggle 함수 인라인 작성
    - 상수 > 타입 > 상태 > 라우터 > useEffect > 이벤트 핸들러로 순서 조정

--- panda-market/src/components/common/DropDown/index.tsx
+++ panda-market/src/components/common/DropDown/index.tsx
@@ -5,28 +5,8 @@ import { useRouter } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
 
 export default function DropDown() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
   const router = useRouter();
   const dropDownRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleCheckClickOutside = (event: MouseEvent) => {
-      if (dropDownRef.current && !dropDownRef.current.contains(event.target as Node)) {
-        setIsOpen(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleCheckClickOutside);
-
-    // clean up
-    return () => {
-      document.removeEventListener('mousedown', handleCheckClickOutside);
-    };
-  }, []);
-
-  const handleDropDownToggle = () => {
-    setIsOpen((prev) => !prev);
-  };
 
   const dropDownOptions = [
     { id: 'recent', label: '최신순' },
@@ -35,18 +15,35 @@ export default function DropDown() {
 
   type OptionType = 'recent' | 'like';
 
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  // 선택된 드롭다운 옵션에 따라 경로 쿼리 스트링 업데이트
   const handleDropDownOptionClick = (selectedOption: OptionType) => {
     const params = new URLSearchParams(window.location.search);
     params.set('orderBy', selectedOption);
     router.push(`?${params}`);
   };
 
+  // 드롭다운 외부 클릭 시 닫힘
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropDownRef.current && !dropDownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    // clean up
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="relative" ref={dropDownRef}>
       {/* dropdown handler*/}
       <div
         className="flex h-[42px] w-[130px] cursor-pointer items-center rounded-xl border border-gray-200 bg-white px-5 py-3"
-        onClick={handleDropDownToggle}
+        onClick={() => setIsOpen((prev) => !prev)}
       >
         <div className="flex gap-6">
           <span>최신순</span>
@@ -54,6 +51,7 @@ export default function DropDown() {
         </div>
       </div>
 
+      {/* dropdown menu*/}
       {isOpen && (
         <ul className="absolute top-full z-50 mt-2 flex w-[130px] flex-col items-center justify-center divide-y divide-gray-200 rounded-xl border border-gray-200 bg-white">
           {dropDownOptions.map((option) => (
